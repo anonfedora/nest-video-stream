b50ab313f330bdd9af563cb248e5f64a0299a109
anonfedora <musaeleazar090@gmail.com>
2024-04-09 23:38:24 +0100

    config 001

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { JwtModule } from "@nestjs/jwt";
+import "dotenv/config";
 
 @Module({
-    imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+    imports: [
+        MongooseModule.forRoot(process.env.MONGO_URI),
+        JwtModule.register({
+            secret,
+            signOptions: { expiresIn: "2h" }
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "..", "public")
+        })
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
