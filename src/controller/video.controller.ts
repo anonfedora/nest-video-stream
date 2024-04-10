ab3ce7cb4e8bb4dab0a9972fa463e767bb1abb23
anonfedora <musaeleazar090@gmail.com>
2024-04-10 08:09:04 +0100

    video: service streamVideo

--- src/controller/video.controller.ts
+++ src/controller/video.controller.ts
@@ -49,6 +49,28 @@ export class VideoController {
             newVideo
         });
     }
-    
-    
+
+    @Get()
+    async read(@Query() id): Promise<Object> {
+        return await this.videoService.readVideo(id);
+    }
+
+    @Get("/:id")
+    async stream(@Param("id") id, @Res() res, @Req() req) {
+        return await this.videoService.streamVideo(id, res, req);
+    }
+
+    @Put(":/id")
+    async update(@Res() res, @Param("id") id, @Body() video: Video) {
+        const updateVideo = await this.videoService.update(id);
+        return res.status(HttpStatus.OK).json(updateVideo);
+    }
+
+    @Delete(":/id")
+    delete(@Res() res, @Param("id") id) {
+        await this.videoService.delete(id);
+        return res.status(HttpStatus.OK).json({
+            user: null
+        });
+    }
 }
